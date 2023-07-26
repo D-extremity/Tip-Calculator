285d0f934995d4e6a14efd3662e5a0a0e7914929
Satyam Shrivastav <mysatyam99@gmail.com>
2023-07-26 13:07:52 +0530

    Bug fixes

--- MyBill/app/src/main/java/com/example/mybill/MainActivity.kt
+++ MyBill/app/src/main/java/com/example/mybill/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.mybill
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.textfield.TextInputEditText
-import org.w3c.dom.Text
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -18,19 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            var c = findViewById<TextView>(R.id.ctax)
-            var s = findViewById<TextView>(R.id.stax)
-            var i = findViewById<TextView>(R.id.itax)
+            val c = findViewById<TextView>(R.id.ctax)
+            val s = findViewById<TextView>(R.id.stax)
+            val i = findViewById<TextView>(R.id.itax)
             var t = findViewById<TextView>(R.id.ttax)
-            var amount = findViewById<TextView>(R.id.amount)
-            var discount = findViewById<TextView>(R.id.disamount)
+            val amount = findViewById<TextView>(R.id.amount)
+            val discount = findViewById<TextView>(R.id.disamount)
 
 
             var mrps: String = findViewById<TextInputEditText>(R.id.mrp).text.toString()
 //            Log.e("check", "${mrp}")
-            var cgst = findViewById<TextInputEditText>(R.id.cgst).text.toString()
+            val cgst = findViewById<TextInputEditText>(R.id.cgst).text.toString()
             var sgst = findViewById<TextInputEditText>(R.id.sgst).text.toString()
             var igst = findViewById<TextInputEditText>(R.id.igst).text.toString()
             var dis = findViewById<TextInputEditText>(R.id.dis).text.toString()
@@ -47,22 +45,23 @@ class MainActivity : AppCompatActivity() {
                 var d: String = dis(mrp, dis.toDouble())
 
                 if (switchCompat.isChecked) {
-                    c.setText("CGST% : $00.00")
-                    s.setText("SGST% : $00.00")
+                    c.setText("CGST% :q     - - -")
+                    s.setText("SGST% :      - - -")
                     var itax = ist(mrp, igst.toDouble())
 
-                    i.setText("IGST% : $${itax}")
-                    t.setText("Total GST% : $${itax}")
+                    i.setText("IGST% : ${NumberFormat.getCurrencyInstance().format(itax.toDouble())}")
+                    t.setText("Net GST% : ${NumberFormat.getCurrencyInstance().format(itax.toDouble())}")
 
-                    discount.setText("Discount : $${d}")
+                    discount.setText("Discount : ${NumberFormat.getCurrencyInstance().format(d.toDouble())}")
                     if (add.isEmpty())
                         add = "0"
                     if (ded.isEmpty())
                         ded = "0"
 
                     val pay = String.format("%.2f",-d.toDouble() + itax.toDouble() + add.toDouble() - ded.toDouble()+mrp)
+                    val p=NumberFormat.getCurrencyInstance().format(pay.toDouble())
 
-                    amount.setText("Total Amount : $${pay}")
+                    amount.setText("Total Amount : $p")
 
 
                 } else {
@@ -75,16 +74,18 @@ class MainActivity : AppCompatActivity() {
                         add = "0"
                     if (ded.isEmpty())
                         ded = "0"
-                    i.setText("IGST% : $00.00")
+                    i.setText("IGST% :      - - -")
                     var cst=cst(mrp,cgst.toDouble())
-                    var sst=cst(mrp,sgst.toDouble())
-                    c.setText("CGST% : $${cst}")
-                    s.setText("SGST% : $${sst}")
-                    discount.setText("Discount : $${d}")
-                    t.setText("Total GST% : $${(cst.toDouble()+sst.toDouble()).toString()}")
-                    val pay = String.format("%.2f",-d.toDouble() + cst.toDouble()+sst.toDouble() + add.toDouble() - ded.toDouble()+mrp)
+                    var sst=sst(mrp,sgst.toDouble())
+                    val total=NumberFormat.getCurrencyInstance().format(cst.toDouble()+sst.toDouble())
+                    c.setText("CGST% : ${NumberFormat.getCurrencyInstance().format(cst.toDouble())}")
+                    s.setText("SGST% : ${NumberFormat.getCurrencyInstance().format(sst.toDouble())}")
+                    discount.setText("Discount : ${NumberFormat.getCurrencyInstance().format(d.toDouble())}")
+                    t.setText("Net GST% : ${total}")
+                    val pay=String.format("%.2f",-d.toDouble() + cst.toDouble()+sst.toDouble() + add.toDouble() - ded.toDouble()+mrp).toDouble()
+                    val p = NumberFormat.getCurrencyInstance().format(pay)
 
-                    amount.setText("Total Amount : $${pay}")
+                    amount.setText("Total Amount : ${p}")
                 }
 
 
@@ -96,25 +97,25 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun cst(mrp: Double, c: Double): String {
+    private fun cst(mrp: Double, c: Double): String {
         var s = String.format("%.2f", (mrp * c) / 100)
 
         return s
     }
 
-    fun sst(mrp: Double, c: Double): String {
+    private fun sst(mrp: Double, c: Double): String {
         var s = String.format("%.2f", (mrp * c) / 100)
 
         return s
     }
 
-    fun ist(mrp: Double, c: Double): String {
+    private fun ist(mrp: Double, c: Double): String {
         var s = String.format("%.2f", (mrp * c) / 100)
 
         return s
     }
 
-    fun dis(mrp: Double, c: Double): String {
+    private fun dis(mrp: Double, c: Double): String {
         var s = String.format("%.2f", (mrp * c) / 100)
 
         return s
